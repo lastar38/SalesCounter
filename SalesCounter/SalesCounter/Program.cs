42c89b0ff369cc6a6519403aaf0f8d551e117215
lastar38 <lastarmain@gmail.com>
2021-02-13 15:57:24 +0900

    追加

--- SalesCounter/SalesCounter/Program.cs
+++ SalesCounter/SalesCounter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace SalesCounter
     {
         static void Main(string[] args)
         {
+            SalesCounter sales = new SalesCounter(ReadSales("sales.csv"));
+            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+            foreach (KeyValuePair<string, int> obj in amountPerStore)
+            {
+                Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+            }
+        }
+
+        static List<Sale> ReadSales(string filePath)
+        {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(',');
+                Sale sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
     }
 }
